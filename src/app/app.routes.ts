52000bf9761d539667e27efae7b1e0fc61ca510d
Md Nasirul Haque <talk2lurisan@gmail.com>
2025-05-10 02:34:02 +0530

    setup next-remote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'http://localhost:3000/_next/static/chunks/remoteEntry.js',
+      remoteName: 'ccd2025-home',
+      exposedModule: './home',
+      elementName: 'ccd2025-home',
+    } as WebComponentWrapperOptions
+  },
+  {
+    path: 'user',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'http://localhost:3001/_next/static/chunks/remoteEntry.js',
+      remoteName: 'ccd2025-user',
+      exposedModule: './user',
+      elementName: 'ccd2025-user',
+    } as WebComponentWrapperOptions
+  }
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   name: 'ccd2025-host',
-
-  exposes: {
-    './Component': './src/app/app.component.ts',
+  remotes: {
+    './Home': 'http://localhost:3000/_next/static/chunks/remoteEntry.js',
+    './User': 'http://localhost:3001/_next/static/chunks/remoteEntry.js',
   },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
